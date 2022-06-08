66154864f0050db64a9c9d149d4039a81145da1e
Wagner Hernandez <whernandez0989@gmail.com>
2022-06-08 09:27:53 -0600

    se agrega repositorios

---
+++ PeliculasAPI/Repositorios/RepositorioEnMemoria.cs
@@ -0,0 +1,20 @@
+﻿using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Repositorios
+{
+    public class RepositorioEnMemoria
+    {
+
+        private List<Genero> _generos;
+
+        public RepositorioEnMemoria()
+        {
+            _generos = new List<Genero>()
+            {
+                new Genero() {Id = 1, Nombre = "Comedia"},
+                new Genero() {Id=2, Nombre ="Drama"}
+            };
+        }
+
+    }
+}
